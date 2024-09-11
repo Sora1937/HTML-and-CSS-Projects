9c37e15425185be76bdd5d13f572c6b41c2588c5
Sora1937 <frostbite1937@outlook.com>
2024-09-11 14:06:54 -0700

    i organized the entire repository
    removed a few pointless files, renamed files, made the whole thing easier to navigate

---
+++ Basic_HTML_and_JavaScript_1/Challenges/JS/main.js
@@ -0,0 +1,25 @@
+function validateForm() {
+    let x = document.forms["myForm"]["email"].value;
+    let y = document.forms["myForm"]["number"].value;
+    let z = document.forms["myForm"]["psw"].value;
+    if (x == "") {
+        alert("Name must be filled out");
+        return false;
+    }
+    if (y == "") {
+        alert("Phone Number must be filled out");
+        return false;
+    }
+    if (z == "") {
+        alert("Email must be filled out");
+        return false;
+    }
+}
+
+function openForm() {
+    document.getElementById('myForm').style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
